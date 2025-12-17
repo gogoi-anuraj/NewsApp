cd5b6fa1ecd8ebf1ed97660e715b9adc55d4660f
Anuraj Gogoi <anurajgogoi01@gmail.com>
2025-12-17 12:50:28 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,54 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+import DetailScreen from "./screens/DetailScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import { Pressable, Text } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import SearchScreen from "./screens/SearchScreen";
+import SavedScreen from "./screens/SavedScreen";
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function TabNavigator() {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Tab.Screen name="HomeTab" component={HomeScreen} />
+      <Tab.Screen name="Search" component={SearchScreen} />
+      <Tab.Screen name="Saved" component={SavedScreen} />
+    </Tab.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={TabNavigator}
+          options={({ navigation }) => ({
+            title: "NewsApp",
+            headerStyle: {
+              backgroundColor: "#aaaa",
+            },
+            headerTintColor: " #fff",
+            headerTitleStyle: { fontWeight: "bold" },
+            headerRight: () => (
+              <Pressable onPress={() => navigation.navigate("Profile")}>
+                <Text style={{ color: "#111", fontSize: 16 }}>Profile</Text>
+              </Pressable>
+            ),
+          })}
+        />
+        <Stack.Screen name="Detail" component={DetailScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
